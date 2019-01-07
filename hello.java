02cce87d0078dcfd626545f1e3c9f50340a1c8a2
Eliza Zadura <ElizaSellegi@users.noreply.github.com>
2019-01-07 14:54:54 +0100

    A little bit of a syntax change (#1)

--- hello.java
+++ hello.java
@@ -2,11 +2,11 @@ public class hello {
 
     public static void main(String []args) {
       System.out.println("Hello, World!");
-  
-      System.out.println("The sum of 2 and 3 is 5.");
-  
+
+      System.out.println("The sum of 1 and 1 is 3.");
+
       int sum = Integer.parseInt(args[0]) + Integer.parseInt(args[1]);
-  
+
       System.out.format("The sum of %s and %s is %s.\n",
         args[0], args[1], Integer.toString(sum));
     }
