8d601e8ff7828202d88eb0598ca4904326239829
eliza_zadura <eliza.zadura@sellegi.se>
2019-01-09 12:55:32 +0100

    DCP-4 #resolve

--- webapp/js/main.js
+++ webapp/js/main.js
@@ -1,3 +1,12 @@
 void function() {
-    alert("Hello from IIFE!");
+
+    var list = document.createElement("LI")
+    var ele = Document.getElementById("root")
+
+    list.innerHTML = "And hello again..."
+
+    ele.appendChild(list)
+
+    alert("Hello from IIFE!")
+
 }();
